3aa3a920fb8dfeb8487106c7cbb8f074a719db9b
Captoweb <captainphp@yandex.ru>
2024-01-02 14:18:01 +0300

    lesson 01 (3)

---
+++ lesson-01/03.js
@@ -0,0 +1,22 @@
+// 3 задание
+
+// Имея в распоряжении только:
+// ​let number = 1;
+// и используя лишь
+// --number   number--  ++number   number++
+// Получи в консоли 1,3,3,1 
+
+
+// Нужно вызвать консоль ЧЕТЫРЕ раза
+// Пример:
+// console.log(number**); 
+// console.log(**number); 
+// console.log(number**); 
+// console.log(**number); 
+
+let number = 1;
+console.log('03.js'); 
+console.log(number++); 
+console.log(++number); 
+console.log(number--) 
+console.log(--number); 
