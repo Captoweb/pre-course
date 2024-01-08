db42c433a071dcd3c9ee83de192bf0f7cd797f80
Captoweb <captainphp@yandex.ru>
2024-01-08 23:31:47 +0300

    lesson 2 (4 update)

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,5 +1,3 @@
-// Исправь функцию так что бы она работала? и получи 'Something wrong!' передав числа, которые вызовут это условие. Обрати внимание здесь «и» (&&), а не или(||)
-
 const giveMeRightCondition = (value1, value2, maximum) => {
     if (value1 == value2 && value1 <= maximum) {
         console.log('Success!');
